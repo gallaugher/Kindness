848757b5af824889f871dae0cf5953f077fa441f
John Gallaugher <john.gallaugher@bc.edu>
2020-06-21 17:14:26 -0400

    Initial Commit

---
+++ Kindness/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kindness
+//
+//  Created by John Gallaugher on 6/21/20.
+//  Copyright © 2020 John Gallaugher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
