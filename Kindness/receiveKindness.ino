c8d2db1a0cf71ea7b930fa0999ee6be998511660
John Gallaugher <john.gallaugher@bc.edu>
2020-06-25 16:42:16 -0400

    Modified receiveKindness.ino so that it no longer requires the serial monitor to be open. A wireless device can be booted and run from a battery, like a mobile phone charger, and will be completely wireless + online, just like SpaceMonkey: https://youtu.be/L0b35TN9qpc

--- Kindness/receiveKindness.ino
+++ Kindness/receiveKindness.ino
@@ -1,11 +1,4 @@
-/* Arduino Script for an arduino 33 IOT device.
-# Will flash a light attached to pin #3
-# A simple modification of Tom Igoe's MqttClientReceiver.ino
-# example at ITP Camp 2020. I didn't clean out Tom's old code.
-# Be sure to change the topic in:
-# char topic[] = "A0mrv2xLUFd5ZgDfdZy2M0RrpCJ2";
-# to the user's unique uID from Cloud Firestore
-*/
+/*
   MQTT Client with an LED
 
   This sketch demonstrates an MQTT client that connects to a broker, subsrcibes to a topic,
@@ -29,6 +22,18 @@
   created 11 June 2020
   by Tom Igoe
 */
+//#define BEATTIME 200
+//#define SPEAKER 12
+
+// Neopixel Stuff
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#define PIN 3
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(16, PIN, NEO_GRBW + NEO_KHZ800);
+
 
 #include <WiFiNINA.h>
 #include <ArduinoMqttClient.h>
@@ -41,7 +46,7 @@ MqttClient mqttClient(wifi);
 // details for MQTT client:
 char broker[] = "broker.shiftr.io";
 int port = 1883;
-char topic[] = "YOUR_UNIQUE_GOOGLE_UID_HERE";
+char topic[] = "A0mrv2xLUFd5ZgDfdZy2M0RrpCJ2";
 char clientID[] = "ledClient";
 
 // number value of incoming message that will light the LED:
@@ -53,10 +58,15 @@ int intensity = 0;
 const int ledPin = 3;
 
 void setup() {
+  // block below added for neopixels:
+  strip.begin();
+  strip.setBrightness(50);
+  strip.show(); // Initialize all pixels to 'off'
+  
   // initialize serial:
   Serial.begin(9600);
   // wait for serial monitor to open:
-  while (!Serial);
+  // while (!Serial);
 
   // initialize I/O pin:
   pinMode (ledPin, OUTPUT);
@@ -95,10 +105,36 @@ void loop() {
   if (mqttClient.parseMessage() > 0) {
     Serial.print("Got a message on topic: ");
     Serial.println(mqttClient.messageTopic());
-    digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(1000);                       // wait for a second
-    digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
-    delay(1000);                       // wait for a second
+
+    for (int i = 0; i<=3; i++) {
+      colorWipe(strip.Color(255, 0, 0), 50); // Red
+      strip.clear();
+      strip.show();
+    }
+    
+
+
+// Old breadboard code below
+//    tone(SPEAKER, 262, BEATTIME); // Do
+//    delay(BEATTIME);
+//    tone(SPEAKER, 294, BEATTIME); // Re
+//    delay(BEATTIME);
+//    tone(SPEAKER, 330, BEATTIME); // Mi
+//    delay(BEATTIME);
+//    tone(SPEAKER, 349, BEATTIME); // Fa
+//    delay(BEATTIME);
+//    tone(SPEAKER, 392, BEATTIME); // So
+//    delay(BEATTIME);
+//    tone(SPEAKER, 440, BEATTIME); // La
+//    delay(BEATTIME);
+//    tone(SPEAKER, 494, BEATTIME); // Ti
+//    delay(BEATTIME);
+//    tone(SPEAKER, 523, BEATTIME); // Do
+//    delay(BEATTIME);
+//    digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
+//    delay(1000);                       // wait for a second
+//    digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
+//    delay(1000);                       // wait for a second
     // read the message:
     while (mqttClient.available()) {
       // convert numeric string to an int:
@@ -132,4 +168,90 @@ boolean connectToBroker() {
   mqttClient.subscribe(topic);
   // return that you're connected:
   return true;
+}
+
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, c);    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
